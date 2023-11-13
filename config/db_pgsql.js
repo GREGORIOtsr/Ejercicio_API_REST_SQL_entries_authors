28d6e3cf648667f3789850f4926e7262fdaf7cba
GREGORIOtsr <gralfonsotr@gmail.com>
2023-11-13 21:33:49 +0100

    added vercel.json/connection with elephant

--- config/db_pgsql.js
+++ config/db_pgsql.js
@@ -1,13 +1,18 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
-require('dotenv').config()
+require("dotenv").config();
 
-// Datos de conexión
-const pool = new Pool({ 
+// ElephantSQL connection
+const pool = new Pool({
+  connectionString: process.env.CONNECTION_STRING,
+});
+
+// Local connection
+/* const pool = new Pool({ 
     user: process.env.DB_USER, 
     host: process.env.DB_HOST, 
     database: process.env.DB_DATABASE, 
     password: process.env.DB_PASSWORD
-})
+}) */
 
 module.exports = pool;
